e673d6465991ebe11fc500703faf6cb675504137
pablo.gonzales.2009 <66168769+pablogonzales2009@users.noreply.github.com>
2021-09-02 21:25:38 +0200

    ok commit
    weeps

--- main.js
+++ main.js
@@ -1,2 +1,4 @@
 const Discord = require('discord.js');
 const testmessages = ["example1", "example2", "example3"];
+
+var randmessage = testmessages[Math.floor(Math.random()*testmessages.length)]; // this is my first attempt at javascript dont blame me if this shit dont work
