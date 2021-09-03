bd0a5491934a38a05c4125cc36d29428f5180342
pablo.gonzales.2009 <66168769+pablogonzales2009@users.noreply.github.com>
2021-09-03 14:48:40 +0200

    balling 2
    f

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
-const Discord = require('discord.js'); // test 2
+const discord = require('discord.js'); // test 2
+const client = new Discord.Client();
 const testmessages = ["example1", "example2", "example3"];
 
 var randmessage = testmessages[Math.floor(Math.random()*testmessages.length)]; // this is my first attempt at javascript dont blame me if this shit dont work
