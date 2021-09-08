5b60360d99b0ecefaaf26ed0f389cf780873109e
pablo.gonzales.2009 <66168769+pablogonzales2009@users.noreply.github.com>
2021-09-08 21:16:02 +0200

    braync
    braync

--- main.js
+++ main.js
@@ -1,2 +1,7 @@
-// starting work next day
-const Discord = require('discord.js');
+const discord = require('discord.js'); // test 2
+const client = new Discord.Client();
+const testmessages = ["example1", "example2", "example3"];
+
+var randmessage = testmessages[Math.floor(Math.random()*testmessages.length)]; // this is my first attempt at javascript dont blame me if this shit dont work
+
+client.login("your token here bro!");
